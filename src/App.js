a517daf6b5785fe0dfb5a2703b39e30eaf1c361a
olumayokunayo <olumayokunayo@gmail.com>
2024-02-01 00:19:09 +0000

    pre-nav

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  );
+}
+
+export default App;
